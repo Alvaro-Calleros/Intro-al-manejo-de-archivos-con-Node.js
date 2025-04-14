92b75bd2382d8f1c981bf5ace9b1a631b11dfa45
ALVARO CALLEROS SANCHEZ <2024030359@upsin.edu.mx>
2025-04-14 09:40:06 -0700

    Se creo el gestorNotas.js con Node.js utilizando las practicas vistas en clase

---
+++ gestorNotas.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const filePath = './notas.json';
+
+function agregarNota(titulo, contenido) {
+  let notas = [];
+
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf8');
+
+    if (data) {
+      notas = JSON.parse(data);
+    }
+  }
+  const nuevaNota = { titulo, contenido };
+  notas.push(nuevaNota);
+
+  fs.writeFileSync(filePath, JSON.stringify(notas, null, 2));
+  console.log('Nota agregada con éxito.');
+}
+
+function listarNotas() {
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const notas = JSON.parse(data);
+    console.log(notas);
+  } else {
+    console.log('No hay notas guardadas.');
+  }
+}
+
+function eliminarNota(titulo) {
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf8');
+    let notas = JSON.parse(data);
+    const notasRestantes = notas.filter(nota => nota.titulo !== titulo);
+
+    fs.writeFileSync(filePath, JSON.stringify(notasRestantes, null, 2));
+    console.log(`Nota con título "${titulo}" eliminada.`);
+  } else {
+    console.log('No hay notas para eliminar.');
+  }
+}
+
+agregarNota('Compras', 'Comprar leche y pan.');
+
+listarNotas();
+
+eliminarNota('Compras');
